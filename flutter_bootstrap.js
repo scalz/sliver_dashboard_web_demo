f99d1419c9e1dc0e30051ac734fbdec4399f6797
scalz <scalz@users.noreply.github.com>
2026-07-21 19:17:05 +0000

    Deploying to main from @ scalz/sliver_dashboard@0f0e76840152ddab5362413d88d2d33ae8883257 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"69c8c61792f04cc809dfef0c910414fb9afc06
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3866096445" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1059775538" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
